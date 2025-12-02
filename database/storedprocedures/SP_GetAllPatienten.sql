85c3444a455ab14aa4481c0efe7f761cee7127b6
Mounir1755 <mounirbalikane@outlook.com>
2025-12-02 09:49:48 +0100

    StoredProcedure now gets all patients and sorts on rolename

--- database/storedprocedures/SP_GetAllPatienten.sql
+++ database/storedprocedures/SP_GetAllPatienten.sql
@@ -1,15 +1,20 @@
 DELIMITER $$
 
 DROP PROCEDURE IF EXISTS SP_GetAllPatienten $$
-
-MENEER IK BEN NOG BEZIG :(
-
 CREATE PROCEDURE SP_GetAllPatienten ()
 BEGIN
     SELECT
-         p.patientid
-        ,p.nummer
-        ,p.medischdossier
-    JOIN users AS
+        p.persoonid,
+        u.id AS user_id,
+        u.name AS username,
+        u.email,
+        p.nummer,
+        p.medischdossier
     FROM patient AS p
+    JOIN users AS u ON p.persoonid = u.id
+    JOIN role_user AS ru ON ru.user_id = u.id
+    JOIN roles AS r ON r.id = ru.role_id
+    WHERE r.name = 'klant';
 END $$
+
+DELIMITER ;
