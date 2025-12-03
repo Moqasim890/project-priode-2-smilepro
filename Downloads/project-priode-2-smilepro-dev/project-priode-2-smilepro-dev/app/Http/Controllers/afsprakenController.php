11612e9d9a1d865b3572b946d64a09f882282c3a
MO Mohammed <340452@student.mboutrecht.nl>
2025-12-03 16:41:57 +0100

    Aantal afspraken bekijken

--- Downloads/project-priode-2-smilepro-dev/project-priode-2-smilepro-dev/app/Http/Controllers/afsprakenController.php
+++ Downloads/project-priode-2-smilepro-dev/project-priode-2-smilepro-dev/app/Http/Controllers/afsprakenController.php
@@ -14,23 +14,23 @@ class AfsprakenController extends Controller
     public function index()
     {
         // Roep model method aan - DB query zit in model
-        $afspraken = Afspraken::getAllWithNames();
-        return view('afspraken.index', compact('afspraken'));
+        $afspraken = Afspraken::getAllWithNames();// Haal alle afspraken op met patient- en medewerker namen
+        return view('afspraken.index', compact('afspraken'));// Stuur data naar view
     }
 
     public function show($id)
     {
-        $afspraak = Afspraken::findOrFail($id);
-        return view('afspraken.show', compact('afspraak'));
+        $afspraak = Afspraken::findOrFail($id);// Zoek afspraak op ID of geef 404
+        return view('afspraken.show', compact('afspraak'));// Stuur data naar view
     }
 
-    public function store(Request $request)
+    public function store(Request $request) // Sla nieuwe afspraak op
     {
-        $validated = $request->validate([
-            'patientid' => 'required',
-            'medewerkerid' => 'required',
-            'datum' => 'required|date',
-            'tijd' => 'required',
+        $validated = $request->validate([// Validate input data
+            'patientid' => 'required',// 'patientid' is required
+            'medewerkerid' => 'required',// 'medewerkerid' is required
+            'datum' => 'required|date',// 'datum' is required and must be a date
+            'tijd' => 'required',// 'tijd' is required
         ]);
 
         Afspraken::create(array_merge($validated, ['status' => 'Bevestigd']));
@@ -41,11 +41,11 @@ class AfsprakenController extends Controller
 
     public function update(Request $request, $id)
     {
-        $validated = $request->validate([
-            'datum' => 'required|date',
-            'tijd' => 'required',
-            'status' => 'required',
-            'opmerking' => 'nullable',
+        $validated = $request->validate([// Validate input data
+            'datum' => 'required|date',// 'datum' is required and must be a date
+            'tijd' => 'required',// 'tijd' is required
+            'status' => 'required',// 'status' is required
+            'opmerking' => 'nullable',// 'nullable' allows null values
         ]);
 
         $afspraak = Afspraken::findOrFail($id);
